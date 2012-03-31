2664ce7292553a25a3f5ae18cb139d0aa35a1360
Jason Rose <jason.rose@meltmedia.com>
2012-03-30 21:58:28 -0700

    Creating basic stuff

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject ci "1.0.0-SNAPSHOT"
+  :description "Continuous Integration tooling"
+  :url "https://github.com/jasonrose/ci-appstate"
+  :source-path "src/main/clojure"
+  :compile-path "target/classes"
+  :resources-path "src/main/resources"
+  :test-path "src/test/clojure"
+  :library-path "target/lib"
+  :warn-on-reflection false
+  :main ci.init
+  :repl-init ci.init
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.3"]
+                 [org.clojure/data.json "0.1.2"]])
